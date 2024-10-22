eaf87e6cb39ac3fb324643aabd673d2107911ca6
Rizwan-ReNote <rizwanm@renote.ai>
2024-10-22 15:11:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import ollama
+from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.post("/ask-image/")
+async def ask_image(
+    file: UploadFile = File(...),
+    question: str = Form(...)
+):
+
+    try:
+
+        image_data = file.file.read()
+
+        res = ollama.chat(
+            model="llava",
+            messages=[
+                {
+                    'role': 'user',
+                    'content': question,
+                    # Pass the image data (binary content)
+                    'images': [image_data]
+                }
+            ]
+        )
+
+        # Return the response content from the model
+        return JSONResponse(content={"response": res['message']['content']})
+
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
