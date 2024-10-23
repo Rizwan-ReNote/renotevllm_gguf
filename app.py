604499fedc68b471d113b7bf0bcb05e453096cff
Bharadwaj172 <kamtambharadwaj674@gmail.com>
2024-10-23 15:26:51 +0530

    changes

--- app.py
+++ app.py
@@ -1,41 +1,36 @@
+from typing import Optional
 import ollama
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-
 @app.post("/ask-image/")
 async def ask_image(
-    file: UploadFile = File(None),  
+    file: Optional[UploadFile] = File(None),
     question: str = Form(...)
 ):
-
     try:
-
+        
         image_data = None
+
         
         if file:
-            image_data = file.file.read()
-            
+            image_data = await file.read()
+
+        
         messages = [{'role': 'user', 'content': question}]
+
         
         if image_data:
-            messages[0]['images'] = [image_data]    
+            messages[0]['images'] = [image_data]
 
+        
         res = ollama.chat(
             model="aiden_lu/minicpm-v2.6:Q4_K_M",
-            messages=[
-                {
-                    'role': 'user',
-                    'content': question,
-                    # Pass the image data (binary content)
-                    'images': [image_data]
-                }
-            ]
+            messages=messages
         )
 
-        # Return the response content from the model
         return JSONResponse(content={"response": res['message']['content']})
 
     except Exception as e:
